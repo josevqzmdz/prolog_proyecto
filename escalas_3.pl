6c76b3750101f80fce156b055491d92acd28f157
josevqzmdz <38343804+josevqzmdz@users.noreply.github.com>
2020-06-12 20:09:06 -0500

    Add files via upload

---
+++ escalas_3.pl
@@ -0,0 +1,212 @@
+% C
+scale(c, major, [c, d, e, f, g, a, b]).
+scale(c, lydian, [c, d, e, f_s, g, a, b]).
+scale(c, mixolydian, [c, d, e, f, g, a, b_b]).
+
+scale(c, minor, [c, d, e_b, f, g, a_b, b_b]).
+scale(c, dorian, [c, d, e_b, f, g, a, b_b]).
+scale(c, phrygian, [c, d_b, e_b, f, g, a_b, b_b]).
+
+scale(c, locrian, [c, d_b, e_b, f, g_b, a_b, b]).
+
+
+% C#
+scale(c_s, major, [c_s, d_s, e_s, f_s, g_s, a_s, b_s]).
+scale(c_s, lydian, [c_s, d_s, e_s, f_ss, g_s, a_s, b_s]).
+scale(c_s, mixolydian, [c_s, d_s, e_s, f_s, g_s, a_s, b]).
+
+scale(c_s, minor, [c_s, d_s, e, f_s, g_s, a, b]).
+scale(c_s, dorian, [c_s, d_s, e, f_s, g_s, a_s, b]).
+scale(c_s, phrygian, [c_s, d, e, f_s, g_s, a, b]).
+
+scale(c_s, dorian, [c_s, d, e, f_s, g, a, b]).
+
+
+% Db
+scale(d_b, major, [d_b, e_b, f, g_b, a_b, b_b, c]).
+scale(d_b, lydian, [d_b, e_b, f, g, a_b, b_b, c]).
+scale(d_b, mixolydian, [d_b, e_b, f, g_b, a_b, b_b, c_b]).
+
+scale(d_b, minor, [d_b, e_b, f_b, g_b, a_b, b_bb, c_b]).
+scale(d_b, dorian, [d_b, e_b, f_b, g_b, a_b, b_b, c_b]). 
+scale(d_b, phrygian, [d_b, e_bb, f_b, g_b, a_b, b_bb, c_b]).
+
+scale(d_b, locrian, [d_b, e_bb, f_b, g_b, a_bb, b_bb, c_b]).
+
+% D
+scale(d, major, [ d, e, f_s, g, a, b, c_s]).
+scale(d, lydian, [ d, e, f_s, g_s, a, b, c_s]).
+scale(d, mixolydian, [ d, e, f_s, g, a, b, c]).
+
+scale(d, minor, [d, e, f, g, a, b_b, c]).
+scale(d, dorian, [d, e, f, g, a, b, c]).
+scale(d, phrygian, [d, e_b, f, g, a, b_b, c]).
+
+scale(d, locrian, [ d, e_b, f, g, a_b, b_b, c]).
+
+% D#
+scale(d_s, major, [d_s, e_s, f_ss, g_s, a_s, b_s, c_ss]).
+scale(d_s, lydian, [d_s, e_s, f_ss, g_ss, a_s, b_s, c_ss]).
+scale(d_s, mixolydian, [d_s, e_s, f_ss, g_s, a_s, b_s, c_s]).
+
+scale(d_s, minor, [d_s, e_s, f_s, g_s, a_s, b, c_s]).
+scale(d_s, dorian, [d_s, e_s, f_s, g_s, a_s, b_s, c_s]).
+scale(d_s, phrygian, [d_s, e, f_s, g_s, a_s, b, c_s]).
+
+scale(d_s, locrian, [d_s, e, f_s, g_s, a, b, c_s]).
+
+% Eb
+scale(e_b, major, [e_b, f, g, a_b, b_b, c, d]).
+scale(e_b, lydian, [e_b, f, g, a, b_b, c, d]).
+scale(e_b, mixolydian, [e_b, f, g, a_b, b_b, c, d_b]).
+
+scale(e_b, minor, [e_b, f, g_b, a_b, b_b, c_b, d_b]).
+scale(e_b, dorian, [e_b, f, g_b, a_b, b_b, c, d_b]).
+scale(e_b, phrygian, [e_b, f_b, g_b, a_b, b_b, c_b, d_b]).
+
+scale(e_b, locrian, [e_b, f_b, g_b, a_b, b_bb, c_b, d_b]).
+
+% E
+scale(e, major, [e, f_s, g_s, a, b, c_s, d_s]).
+scale(e, lydian, [e, f_s, g_s, a_s, b, c_s, d_s]).
+scale(e, mixolydian, [e, f_s, g_s, a, b, c_s, d]).
+
+scale(e, minor, [e, f_s, g, a, b, c, d]).
+scale(e, dorian, [e, f_s, g, a, b, c_s, d]).
+scale(e, phrygian, [e, f, g, a, b, c, d]).
+
+scale(e, locrian, [e, f, g, a, b_b, c, d]).
+
+% E#
+scale(e_s, major, [e_s, f_ss, g_ss, a_s, b_s, c_ss, d_ss]).
+scale(e_s, lydian, [e_s, f_ss, g_ss, a_ss, b_s, c_ss, d_ss]).
+scale(e_s, mixolydian, [e_s, f_ss, g_ss, a_s, b_s, c_ss, d_s]).
+
+scale(e_s, minor, [e_s, f_ss, g_s, a_s, b_s, c_s, d_s]).
+scale(e_s, dorian, [e_s, f_ss, g_s, a_s, b_s, c_ss, d_s]).
+scale(e_s, phrygian, [e_s, f_s, g_s, a_s, b_s, c_s, d_s]).
+
+scale(e_s, locrian, [e_s, f_s, g_s, a_s, b, c_s, d_s]).
+
+
+% Fb
+scale(f_b, major, [f_b, g_b, a_b, b_bb, c_b, d_b, e_b]).
+scale(f_b, lydian, [f_b, g_b, a_b, b_b, c_b, d_b, e_b]).
+scale(f_b, mixolydian, [f_b, g_b, a_b, b_bb, c_b, d_b, e]).
+
+scale(f_b, minor, [f_b, g_b, a_bb, b_bb, c_b, d_bb, e_bb]).
+scale(f_b, dorian, [f_b, g_b, a_bb, b_bb, c_b, d_b, e_bb]).
+scale(f_b, phrygian, [f_b, g, a_bb, b_bb, c_b, d_bb, e_bb]).
+
+scale(f_b, locrian, [f_b, g_bb, a_bb, b_bb, c_bb, d_bb, e_bb]).
+
+% F
+scale(f, major, [f, g, a, b_b, c, d, e]).
+scale(f, lydian, [f, g, a, b, c, d, e]).
+scale(f, mixolydian, [f, g, a, b_b, c, d, e_b]).
+
+scale(f, minor, [f, g, a_b, b_b, c, d_b, e]).
+scale(f, dorian, [f, g, a_b, b_b, c, d, e_b]).
+scale(f, phrygian, [f, g_b, a_b, b_b, c, d_b, e_b]).
+
+scale(f, locrian, [f, g_b, a_b, b_b, c_b, d_b, e_b]).
+
+% Gb
+scale(g_b, major, [g_b, a_b, b_b, c_b, d_b, e_b, f]).
+scale(g_b, lydian, [g_b, a_b, b_b, c, d_b, e_b, f]).
+scale(g_b, mixolydian, [g_b, a_b, b_b, c_b, d_b, e_b, f_b]).
+
+scale(g_b, minor, [g_b, a_b, b_bb, c_b, d_b, e_bb, f_b]).
+scale(g_b, dorian, [g_b, a_b, b_bb, c_b, d_b, e_b, f_b]).
+scale(g_b, phrygian, [g_b, a_bb, b_bb, c_b, d_b, e_bb, f_b]).
+
+scale(g_b, locrian, [g_b, a_bb, b_bb, c_b, d_bb, e_bb, f_b]).
+
+% G
+scale(g, major, [g, a, b, c, d, e, f_s]).
+scale(g, lydian, [g, a, b, c_s, d, e, f_s]).
+scale(g, mixolydian, [g, a, b, c, d, e, f]).
+
+scale(g, minor, [g, a, b_b, c, d, e_b, f]).
+scale(g, dorian, [g, a, b_b, c, d, e, f]).
+scale(g, phrygian, [g, a_b, b_b, c, d, e_b, f]).
+
+scale(g, locrian, [g, a_b, b_b, c, d_b, e_b, f]).
+
+% G#
+scale(g_s, major, [g_s, a_s, b_s, c_s, d_s, e_s, f_ss]).
+scale(g_s, lydian, [g_s, a_s, b_s, c_ss, d_s, e_s, f_ss]).
+scale(g_s, mixolydian, [g_s, a_s, b_s, c_s, d_s, e_s, f_s]).
+
+scale(g_s, minor, [g_s, a_s, b, c_s, d_s, e, f_ss]).
+scale(g_s, dorian, [g_s, a_s, b, c_s, d_s, e_s, f_s]).
+scale(g_s, phrygian, [g_s, a, b, c_s, d_s, e, f_s]).
+
+scale(g_s, locrian, [g_s, a, b, c_s, d, e, f_s]).
+
+% Ab
+scale(a_b, major, [a_b, b_b, c, d_b, e_b, f, g]).
+scale(a_b, lydian, [a_b, b_b, c, d, e_b, f, g]).
+scale(a_b, mixolydian, [a_b, b_b, c, d_b, e_b, f, g_b]).
+
+scale(a_b, minor, [a_b, b_b, c_b, d_b, e_b, f_b, g_b]).
+scale(a_b, dorian, [a_b, b_b, c_b, d_b, e_b, f, g_b]).
+scale(a_b, minor, [a_b, b_bb, c_b, d_b, e_b, f_b, g_b]).
+
+scale(a_b, locrian, [a_b, b_bb, c_b, d_b, e_bb, f_b, g_b]).
+
+% A
+scale(a, major, [a, b, c_s, d, e, f_s, g_s]).
+scale(a, lydian, [a, b, c_s, d_s, e, f_s, g_s]).
+scale(a, mixolydian, [a, b, c_s, d, e, f_s, g]).
+
+scale(a, minor, [a, b, c, d, e, f, g]).
+scale(a, dorian, [a, b, c, d, e, f_s, g]).
+scale(a, phrygian, [a, b_b, c, d, e, f, g]).
+
+scale(a, locrian, [a, b_b, c, d, e_b, f, g]).
+
+% A#
+scale(a_s, major, [a_s, b_s, c_ss, d_s, e_s, f_ss, g_ss]).
+scale(a_s, lydian, [a_s, b_s, c_ss, d_ss, e_s, f_ss, g_ss]).
+scale(a_s, mixolydian, [a_s, b_s, c_ss, d_s, e_s, f_ss, g_s]).
+
+scale(a_s, minor, [a_s, b_s, c_s, d_s, e_s, f_s, g_s]).
+scale(a_s, dorian, [a_s, b_s, c_s, d_s, e_s, f_ss, g_s]).
+scale(a_s, phrygian, [a_s, b, c_s, d_s, e_s, f_s, g_s]).
+
+scale(a_s, locrian, [a_s, b, c_s, d_s, e, f_s, g_s]).
+
+% Bb
+scale(b_b, major, [b_b, c, d, e_b, f, g, a]).
+scale(b_b, lydian, [b_b, c, d, e, f, g, a]).
+scale(b_b, mixolydian, [b_b, c, d, e_b, f, g, a_b]).
+
+scale(b_b, minor, [b_b, c, d_b, e_b, f, g_b, a_b]).
+scale(b_b, dorian, [b_b, c, d_b, e_b, f, g, a_b]).
+scale(b_b, phrygian, [b_b, c_b, d_b, e_b, f, g_b, a_b]).
+
+scale(b_b, locrian, [b_b, c_b, d_b, e_b, f_b, g_b, a_b]).
+
+% B
+scale(b, major, [b, c_s, d_s, e, f_s, g_s, a_s]).
+scale(b, lydian, [b, c_s, d_s, e_s, f_s, g_s, a_s]).
+scale(b, mixolydian, [b, c_s, d_s, e, f_s, g_s, a]).
+
+scale(b, minor, [b, c_s, d, e, f_s, g, a]).
+scale(b, dorian, [b, c_s, d, e, f_s, g_s, a]).
+scale(b, phrygian, [b, c, d, e, f_s, g, a]).
+
+scale(b, locrian, [b, c, d, e, f, g, a]).
+
+% B#
+scale(b_s, major, [b_s, c_ss, d_ss, e_s, f_ss, g_ss, a_ss]).
+scale(b_s, major, [b_s, c_ss, d_s, e_s, f_ss, g_s, a_s]).
+
+% algoritmo de busqueda
+search(Note, Scale, Item) :-
+    scale(Note, Scale, Items),
+    member(Item, Items).
+	
+
+
